baba2a5fc1221f7eb45bc445a6a022c0adb34a3b
Hector Bedoya <hectorbed@Hectors-MacBook-Pro.local>
2024-07-20 10:15:59 -0500

    SpringBoot project with README

---
+++ README.md
@@ -0,0 +1,6 @@
+**Assignment**: Patrones de Diseño
+
+Integrantes del grupo:
+- Inerjanuer Bernate Fonseca
+- Ivan Dario Viasus Quintero
+- Hector Fabio Bedoya Loaiza
---
+++ src/test/java/com/unisabana/patrones/PatronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisabana.patrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
